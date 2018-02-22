2aa936aee482f22b87d9986eb3e0d3ced8750eeb
Alexander Morozov <alexander.morozov@postindustria.com>
2018-02-22 09:57:56 +0200

    Fixed all rubocop warnings. Moved rubocop verification to a spec.

--- spec/features/home_page_feature_spec.rb
+++ spec/features/home_page_feature_spec.rb
@@ -1,5 +1,5 @@
-RSpec.feature "Home page content", :type => :feature do
-  scenario "Check content on a home page" do
+RSpec.feature 'Home page content', type: :feature do
+  scenario 'Check content on a home page' do
     visit '/'
     expect(page).to have_content 'First page of the app'
   end
