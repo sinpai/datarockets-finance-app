600404594368dad0fbcd5415a28b91aaa44d7714
Alexander Morozov <alexander.morozov@postindustria.com>
2018-02-28 22:17:44 +0200

    Added ability to add avatar for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
     :validatable, :confirmable, :omniauthable
   devise :omniauthable, omniauth_providers: %i[google_oauth2 github]
