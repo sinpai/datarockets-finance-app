cf5be8ddb51570e2ce56a1ab00507677a9314041
Alexander Morozov <alexander.morozov@postindustria.com>
2018-02-28 22:29:57 +0200

    Added username to user (able to define at edit user page)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,13 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password,
-      :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password,
-      :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar) }
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:username, :email, :password,
+        :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+      user.permit(:username, :email, :password,
+        :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar)
+    end
   end
 end
