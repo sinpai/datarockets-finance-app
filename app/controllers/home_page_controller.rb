2867fa83cc4aaca2b1540b96a8d6cdf30b9b0906
Alexander Morozov <alexander.morozov@postindustria.com>
2018-03-04 12:12:49 +0200

    Fixed notes from mentor (authorize method, i18n, added test for unlogged)

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -3,14 +3,4 @@ class HomePageController < ApplicationController
 
   def show
   end
-
-  protected
-
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to new_user_session_path, notice: 'You should be logged in order to see this page'
-    end
-  end
 end
