fae080b983a09eb1ea36f1e07ba5bffedfab7c50
Alexander Morozov <alexander.morozov@postindustria.com>
2018-03-09 10:09:01 +0200

    Additional fixes against offencies and code smells

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,12 @@
 module ApplicationHelper
-  def will_paginate(collection_or_options = nil, default_paginate_options)
+  DEFAULT_RENDERER_PAGINATE = WillPaginate::ActionView::Bootstrap4LinkRenderer
+
+  def will_paginate(collection_or_options = nil, options = {})
     if collection_or_options.is_a? Hash
       options = collection_or_options
       collection_or_options = nil
     end
-    options = options.merge renderer: BootstrapPagination::Rails unless options[:renderer]
+    options = options.merge renderer: DEFAULT_RENDERER_PAGINATE unless options[:renderer]
     super *[collection_or_options, options].compact
-  end
-
-  private
-
-  def default_paginate_options
-    {renderer: WillPaginate::ActionView::Bootstrap4LinkRenderer}
   end
 end
