198162fdd9b2ee71b6495ff846fb2db080bfc0d1
Alexander Morozov <alexander.morozov@postindustria.com>
2018-03-21 15:14:34 +0200

    Fixed suggestion about user.balance

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :transactions, dependent: :destroy
   has_many :authorizations, dependent: :destroy
 
-  def user_balance
+  def balance
     transactions.sum(:amount).to_i
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe User, type: :model do
     FactoryBot.create_list(:transaction, 10, user: user, amount: 10)
   end
 
-  context '.user_balance' do
+  context '.balance' do
     it 'returns correct total of balance' do
-      expect(user.user_balance).to eq 100
+      expect(user.balance).to eq 100
     end
   end
 end
